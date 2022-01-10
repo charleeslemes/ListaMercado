ccded47e40b5c7ad6a035721a58afd7aba7d5359
Charles Gabriel Lemes <charleslemes21@gmail.com>
2022-01-10 16:20:32 -0300

    css em JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+ var header = document.querySelector('header');
+ var listaCadastro = document.querySelector('.lista-cadastro');
+ var listaCadastroInput = document.querySelector('.lista-cadastro input[type=text]');
+ var listaCadastroInputNumber = document.querySelector('.lista-cadastro input[type=number]');
+ var listaCadastroInputSubmit = document.querySelector('.lista-cadastro input[type=submit]');
+
+
+
+
+
+ header.style.backgroundColor="#5a71bb";
+ header.style.textAlign="center";
+ header.style.color="white";
+ header.style.padding="20px 0";
+
+ /* */
+
+listaCadastro.style.display="flex";
+listaCadastro.style.margin="20px 0";
+listaCadastro.style.padding="0 20px";
+
+/* */
+
+listaCadastroInput.style.borderRadius="20px";
+listaCadastroInput.style.border="1px";
+listaCadastroInput.style.borderStyle="solid";
+listaCadastroInput.style.borderColor="#5a71bb";
+listaCadastroInput.style.flex="1";
+listaCadastroInput.style.height="40px";
+listaCadastroInput.style.paddingLeft="20px";
+
+/* */
+
+listaCadastroInputNumber.style.borderRadius="20px";
+listaCadastroInputNumber.style.border="1px";
+listaCadastroInputNumber.style.borderStyle="solid";
+listaCadastroInputNumber.style.borderColor="#5a71bb";
+listaCadastroInputNumber.style.height="40px";
+listaCadastroInputNumber.style.paddingLeft="20px";
+
+/* */
+
+
+listaCadastroInputSubmit.style.borderRadius="20px";
+listaCadastroInputSubmit.style.cursor="pointer";
+listaCadastroInputSubmit.style.backgroundColor="#5a71bb";
+listaCadastroInputSubmit.style.padding="10px";
+listaCadastroInputSubmit.style.color="white";
+listaCadastroInputSubmit.style.border="0";
+
+
+
 
 
 var items =[];
